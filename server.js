47caac37023bb50dc6523d9febc478c741e561b6
Nirmal <Nirmal@admin.com>
2025-02-15 10:41:03 +0400

    new

--- server.js
+++ server.js
@@ -27,6 +27,6 @@ server.get('/',(req,res)=>{
 
 
 //server listens
-server.listen('0.0.0.0', 3000,()=>{
+server.listen('0.0.0.0', 10000,()=>{
     console.log('Server is running on port 3000');
 });
